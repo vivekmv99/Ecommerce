534f130a1caaa8f8828bbbf10e758a79ef68801f
vivekmv99 <mvvivek99@gmail.com>
2022-06-07 18:52:47 +0530

    setting react

---
+++ home/src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main>
+          <h1>welcome</h1>
+      </main>
+      <Footer />
+       
+    </div>
+  );
+}
+
+export default App;
