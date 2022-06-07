23e93b61747cacde2a01edd9fbcfc606a23c06cd
vivekmv99 <mvvivek99@gmail.com>
2022-06-07 19:57:01 +0530

    header styled

--- home/src/App.js
+++ home/src/App.js
@@ -1,3 +1,5 @@
+import { Container } from 'react-bootstrap'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -5,8 +7,10 @@ function App() {
   return (
     <div>
       <Header/>
-      <main>
+      <main className='py-3'>
+        <Container>
           <h1>welcome</h1>
+        </Container>
       </main>
       <Footer />
        
--- home/src/components/Footer.js
+++ home/src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function Footer() {
   return (
     <div>
-        <footer>footer</footer>
+        <footer>
+          <Container>
+            <Row>
+              <Col className='text-center py-3'>Copyright &copy; Vxvek</Col>
+            </Row>
+          </Container>
+        </footer>
     </div>
   )
 }
