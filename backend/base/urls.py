fefdecc6a6aaf5369b52e577be603322b11b5119
vivekmv99 <mvvivek99@gmail.com>
2022-06-08 22:03:25 +0530

    setting backend rest api

---
+++ backend/base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.gerRoutes, name="routes"),
+    path('products/',views.getProducts, name="products"),
+    path('products/<str:pk>/',views.getProduct, name="product"),
+
+]
