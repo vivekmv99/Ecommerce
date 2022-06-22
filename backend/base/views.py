60bcd6427d671e76916cb171152ccb919957d787
vivekmv99 <mvvivek99@gmail.com>
2022-06-22 22:54:05 +0530

    adding jwt token

--- backend/base/views.py
+++ backend/base/views.py
@@ -7,6 +7,29 @@ from rest_framework.response import Response
 from . models import *
 from . serializers import *
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # data["username"] = self.user.username
+        # data["email"] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+       
+
+        return data
+    
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 @api_view(['GET'])
 def gerRoutes(request):
     routes = [
@@ -25,6 +48,15 @@ def gerRoutes(request):
 
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
 
 @api_view(['GET'])
 def getProducts(request):
