2501998842da3adb0511b61a4be142f1c669488b
vivekmv99 <mvvivek99@gmail.com>
2022-06-23 22:00:49 +0530

    permission setting and signals

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'base',
+    'base.apps.BaseConfig',
 ]
 
 REST_FRAMEWORK = {
---
+++ backend/base/urls/order_urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from base.views import order_views as views
+
+
+urlpatterns = [
+    
+
+   
+
+]
